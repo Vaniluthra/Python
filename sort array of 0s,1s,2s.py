f198f2e14205eecb6fdfc3870b62e001ee5baef1
Vaniluthra <vaniluthra05@gmail.com>
2024-04-05 19:02:00 +0530

    Create sort array of 0s,1s,2s.py

---
+++ sort array of 0s,1s,2s.py
@@ -0,0 +1,15 @@
+a = list(map(int, input('enter space separated list elements:').split()))
+n = len(a)
+c0 = c1 = 0
+for i in a:
+    if i == 0:
+        c0 += 1
+    elif i == 1:
+        c1 += 1
+for i in range(c0):
+    a[i] = 0
+for i in range(c0, c0 + c1):
+    a[i] = 1
+for i in range(c0 + c1, n):
+    a[i] = 2
+print(a)
