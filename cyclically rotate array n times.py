2f8253e12e6b2d3476d7d9a298adcc8a9f3b3ba7
Vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2024-04-27 00:25:27 +0530

    description

--- cyclically rotate array n times.py
+++ cyclically rotate array n times.py
@@ -1,4 +1,8 @@
-m = list(map(int, input('enter space separated list elements:').split()))
+'''
+Description
+Program to cyclically rotate an array n number of times
+'''
+m = list(map(int, input('Enter space separated list elements:').split()))
 n = int(input('n= '))
 n = n % len(m) if n >= len(m) else n
 for i in range(n):
