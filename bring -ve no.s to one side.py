efed35187b96d5117133d1b38aa4edababc080a3
Vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2024-04-27 00:23:46 +0530

    description

--- bring -ve no.s to one side.py
+++ bring -ve no.s to one side.py
@@ -1,4 +1,8 @@
-a = list(map(int, input('enter space separated list elements:').split()))
+'''
+Description
+Program to bring all negative numbers to one side of a list
+'''
+a = list(map(int, input('Enter space separated list elements:').split()))
 c=0
 for i in range(len(a)):
     
