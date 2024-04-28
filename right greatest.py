0db18d421ac88f9067f52f687ae4f52fa1c0642a
Vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2024-04-29 00:12:08 +0530

    Create right greatest.py

---
+++ right greatest.py
@@ -0,0 +1,12 @@
+a=list(map(int,input('enter space separated values:').split()))
+n=len(a)
+for i in range(n-1):
+    for j in range(i+1,n):
+        if a[i]<a[j]:
+            
+            a[i]=a[j]
+            break
+        if j==n-1:
+            a[i]=-1
+a[-1]=-1
+print(a)
