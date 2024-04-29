e177d783f690d13b4467f2508b13e6b44356687c
Vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2024-04-30 00:40:24 +0530

    Update

--- right greatest.py
+++ right greatest.py
@@ -1,9 +1,11 @@
+'''
+
+'''
 a=list(map(int,input('enter space separated values:').split()))
 n=len(a)
 for i in range(n-1):
     for j in range(i+1,n):
         if a[i]<a[j]:
-            
             a[i]=a[j]
             break
         if j==n-1:
