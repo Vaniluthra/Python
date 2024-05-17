5f2e272b3d6b1961c609e9133f1c7e343b8604d7
Vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2024-05-18 01:50:46 +0530

    desc

--- Snake scale to pascal.py
+++ Snake scale to pascal.py
@@ -1,3 +1,7 @@
+'''
+Convert a given string with words separated by underscores into CamelCase format, where each word starts with a capital letter and all spaces and underscores are removed.
+'''
+
 test_str=input()
 print("Original string: " + test_str)
 res = test_str.replace("_", " ").title().replace(" ", "")
